a95eed3225cc3e208af1c2286592622386f0fd0d
saad288 <saad99ie@gmail.com>
2024-12-03 14:02:40 +0500

    bg fix

--- src/App.jsx
+++ src/App.jsx
@@ -6,9 +6,20 @@ import Navbar from './pages/Navbar/Navbar'
 
 function App () {
   return (
-    <>
-      <Navbar />
-    </>
+    <div>
+      <div
+        style={{
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'center'
+        }}
+      >
+        <h2 style={{ color: '#7038FA' }}>Organized By</h2>
+        <div style={{ background: 'gray', padding: 2, borderRadius: 10 }}>
+          <h2 style={{ color: '#7038FA', margin: 0 }}>BUDS X Supercomp</h2>
+        </div>
+      </div>
+    </div>
   )
 }
 
