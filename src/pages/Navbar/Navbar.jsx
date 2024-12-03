f93f03e8b10ec17b8532a26d2bd44aff2571f161
saad288 <saad99ie@gmail.com>
2024-12-03 14:23:57 +0500

    nav fix

--- src/pages/Navbar/Navbar.jsx
+++ src/pages/Navbar/Navbar.jsx
@@ -1,15 +1,15 @@
-import "./navbar.css"
+import './navbar.css'
 
-import logo from "../../assets/img/LOGOCOMPETITIONWHITE-removebg-preview.png"
+import logo from '../../assets/img/LOGOCOMPETITIONWHITE-removebg-preview.png'
 
 const Navbar = () => {
   return (
-    <div className="nav-container">
-      <img src ={logo} style={{width:70,height:70}} />
+    <div className='nav-container'>
+      <img src={logo} style={{ width: 70, height: 70 }} />
       <h1>EVENT</h1>
       <h1>ABOUT US</h1>
       <h1>SUPPORT</h1>
-      <button className="nav-button">CONTACT US</button>
+      <button className='btn-grad'>CONTACT US</button>
     </div>
   )
 }
