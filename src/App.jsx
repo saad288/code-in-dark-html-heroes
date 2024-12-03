8fa12741fa7193f70bfba9dc9e9e1a6ec4142a5c
ALIZA63 <alizakhan99163@gmail.com>
2024-12-03 15:04:32 +0500

    steps

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,11 @@ import './App.css'
 import Navbar from './pages/Navbar/Navbar'
 import Competitions from "./pages/Competitions/Competitions"
 import Partners from './pages/Partner/Partners'
+import Steps from './pages/Steps/Steps'
 
 import bg1 from './assets/img/bg-1.png'
 
-function App () {
+function App() {
   return (
     <div style={{ position: 'relative' }}>
       <Navbar />
@@ -43,6 +44,8 @@ function App () {
         </h2>
       </div>
 
+      <div style={{ marginTop: 20 }}></div>
+
       <img
         src={bg1}
         style={{
@@ -54,8 +57,51 @@ function App () {
         }}
       />
 
+      <div
+        style={{
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'center'
+        }}
+      >
+        <h2 style={{ color: '#7038FA', margin: 0, marginBottom: 4 }}>
+          Our Partners
+        </h2>
+      </div>
+
       <Partners />
+
+      <div
+        style={{
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'center'
+        }}
+      >
+        <h2 style={{ color: 'white', margin: 0, marginBottom: 4 }}>
+          Steps to Follow
+        </h2>
+      </div>
+
+      <Steps />
+
+
+      <div
+        style={{
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'center'
+        }}
+      >
+        <h2 style={{ color: 'white', margin: 0, marginBottom: 4 }}>
+          Competitions
+        </h2>
+      </div>
+
       <Competitions />
+
+
+
     </div>
   )
 }
